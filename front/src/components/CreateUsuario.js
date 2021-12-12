eb30b3e8b018c873d0be2dde566df59ac74a3c89
Jhon Diaz <jhon_0621@hotmail.com>
2021-12-12 17:20:18 -0500

    Crear Usuario

--- front/src/components/CreateUsuario.js
+++ front/src/components/CreateUsuario.js
@@ -3,38 +3,38 @@ import { useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
 const CREATE_USUARIO = gql`
-  mutation CreateUsuario(
-    $email: String!,
-    $numId: String!,
-    $name: String!,
-    $password: String!,
-    $role: String!,
-    $state: String!
-  ) { createUsuario(
-    email: $email,
-    numId: $numId,
-    name: $name,
-    password: $password,
-    role: $role,
-    state: $state){
-        email
-    }
+  mutation CreateUsuario($email: String!, $numId: String!, $name: String!, $password: String!, $role: String!, $state: String!) {
+  createUsuario(input:{
+    email: $email
+    numId: $numId
+    name: $name
+    password: $password
+    role: $role
+    state: $state
+  }){
+    email
+    numId
+    name
+    password
+    role
+    state
   }
+}
 `;
 
 
 const CrearUsuario = () => {
-    
-    const [email, setEmail] = useState("")
-    const [numId, setNumId] = useState("")
-    const [name, setName] = useState("")
-    const [password, setPassword] = useState("")
-    const [role, setRole] = useState("")
-    const [state, setState] = useState("")
 
-    const [createUsuario] = useMutation(CREATE_USUARIO);
+  const [email, setEmail] = useState("")
+  const [numId, setNumId] = useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [role, setRole] = useState("")
+  const [state, setState] = useState("")
 
-    return (
+  const [createUsuario] = useMutation(CREATE_USUARIO)
+  
+  return (
     <div className="row">
       <div className="col-md-6 offset-md-3">
         <div className="card">
@@ -42,11 +42,11 @@ const CrearUsuario = () => {
             <form
               onSubmit={async e => {
                 e.preventDefault();
-                await createUsuario({ variables: { email, numId, name, password, role, state } });
+                await createUsuario({ variables: { email, numId, name, password, role, state } })
                 window.location.href = "/";
               }}
             >
-              <div className="form-group">
+            <div className="form-group">
                 <input
                   type="text"
                   placeholder="email"
@@ -65,40 +65,40 @@ const CrearUsuario = () => {
                 />
               </div>
               <div className="form-group">
-                <textarea
+                <input
                   type="text"
                   placeholder="Nombre"
                   onChange={e => setName(e.target.value)}
                   value={name}
                   className="form-control"
-                ></textarea>
+                ></input>
               </div>
               <div className="form-group">
-                <textarea
+                <input
                   type="text"
                   placeholder="Contraseña"
                   onChange={e => setPassword(e.target.value)}
                   value={password}
                   className="form-control"
-                ></textarea>
+                ></input>
               </div>
               <div className="form-group">
-                <textarea
+                <input
                   type="text"
                   placeholder="Rol"
                   onChange={e => setRole(e.target.value)}
                   value={role}
                   className="form-control"
-                ></textarea>
+                ></input>
               </div>
               <div className="form-group">
-                <textarea
+                <input
                   type="text"
                   placeholder="Estado"
                   onChange={e => setState(e.target.value)}
                   value={state}
                   className="form-control"
-                ></textarea>
+                ></input>
               </div>
               <button className="btn btn-success btn-block">Guardar</button>
             </form>
