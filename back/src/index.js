51413697f89aedb9212ea9fd6699b59e436b2e7d
Jhon Diaz <jhon_0621@hotmail.com>
2021-12-21 20:43:54 -0500

    update

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,6 @@
 import {server} from './server'
 import './graphql/database';
 
-server.start({port: 3100}, ({port}) => {
+server.start({port : process.env.PORT || 3100}, ({port}) => {
   console.log('Server on port', port);
 });
